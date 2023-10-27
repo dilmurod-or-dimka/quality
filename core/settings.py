dd4b8afb10a72739123f3cdd2bed0e740dae5bb4
Burxanov Dilmurod <dilil@gamil.com>
2023-10-27 15:39:13 +0500

    fix settings

--- core/settings.py
+++ core/settings.py
@@ -44,14 +44,6 @@ INSTALLED_APPS = [
     "accounts.apps.AccountsConfig",
 ]
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
-EMAIL_HOST = "imap.gmail.com"
-EMAIL_HOST_USER = "dilmurod1506@gamil.com"
-EMAIL_HOST_PASSWORD = "qua12lity34"
-EMAIL_PORT = 993
-EMAIL_USE_SSL = True
-DEFAULT_FROM_EMAIL = "qualityprofessional15@gmail.com"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
